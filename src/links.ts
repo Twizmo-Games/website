21d21cb0d4b39aa20ac0318e7a7a22b707c7a135
mcbunkus <austenlebeau@gmail.com>
2025-10-21 22:07:04 -0500

    added assets and tattak page

--- src/links.ts
+++ src/links.ts
@@ -2,6 +2,7 @@ import {relPath} from "./util";
 
 export const SiteLinks = [
     {url: relPath("disku"), name: "DISKU"},
+    {url: relPath("taktak"), name: "tak-tak"},
     {url: relPath("about"), name: "About Twizmo!"},
     {url: relPath("contact"), name: "Contact"},
 ];
