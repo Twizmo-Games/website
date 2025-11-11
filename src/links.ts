83efd94817870199449c188b194b869876b5ebc2
mcbunkus <austenlebeau@gmail.com>
2025-11-10 20:27:10 -0600

    made the remaining updates according to Rich's requests.

--- src/links.ts
+++ src/links.ts
@@ -2,7 +2,7 @@ import {relPath} from "./util";
 
 export const SiteLinks = [
     {url: relPath("disku"), name: "DISKU"},
-    {url: relPath("taktak"), name: "tak-tak"},
+    {url: relPath("taktak"), name: "tak•tak"},
     {url: relPath("about"), name: "About Twizmo!"},
     {url: relPath("contact"), name: "Contact"},
 ];
