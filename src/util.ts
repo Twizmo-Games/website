4567c64de9a19aac1b1bae0a312bb0a183d9c7a3
mcbunkus <Austen LeBeau>
2025-12-01 17:19:29 -0600

    added privacy policy page

--- src/util.ts
+++ src/util.ts
@@ -1,17 +1,23 @@
-import { base } from "astro:config/server"
+import { base } from "astro:config/server";
 
 /**
- * 
+ *
  * @param parts path parts that form a complete URL, with the base prepended
  * @returns a fully formed relative URL string
  */
 export const relPath = (...parts: Array<string>): string => {
-    // strips trailing slashes: /base/// -> /base
-    const cleanedBase = base.replace(/\/+$/, '');
+  // strips trailing slashes: /base/// -> /base
+  const cleanedBase = base.replace(/\/+$/, "");
 
-    // strips all leading and trailing slashes: ///about// -> about
-    const cleanedParts = parts.map(p => p.replace(/^\/+|\/+$/g, ''));
+  // strips all leading and trailing slashes: ///about// -> about
+  const cleanedParts = parts.map((p) => p.replace(/^\/+|\/+$/g, ""));
 
+  return [cleanedBase, ...cleanedParts].filter(Boolean).join("/");
+};
 
-    return [cleanedBase, ...cleanedParts].filter(Boolean).join('/');
-}
+/**
+ * @returns the current year as a number
+ */
+export const currentYear = (): number => {
+  return new Date().getFullYear();
+};
